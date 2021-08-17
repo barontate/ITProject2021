6f474e34c2b331b8b7558d50dd65acc805c677bc
Tate Deklerk <tdeklerk@student.unimelb.edu.au>
2021-08-17 14:51:14 +1000

    removed redundant lines.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express();
-const http = require('http');
+
 require("./models")
 
-const hostname = '127.0.0.1';
+
 const port = 3000;
 
 app.listen(port, () => {
-    console.log('The library app is listening on port 3000!')
+    console.log('The library app is listening on port 3000! http://localhost:3000')
 })
 
 app.get('/', (req, res) => {
