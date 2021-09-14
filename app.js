15e500e331f96136712542cf1e29dee91f6a7860
jackwhitehead <whiteheadjack@hotmail.com>
2021-09-14 14:09:51 +1000

    Connected front and back

--- app.js
+++ app.js
@@ -23,8 +23,4 @@ app.get('/', (req, res) => {
 
 app.get('/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-<<<<<<< Updated upstream
-}); 
-=======
-}); 
->>>>>>> Stashed changes
+})
