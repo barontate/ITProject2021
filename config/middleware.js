01feeb5b49aac5787c385acbebca65607908b843
nelsonwalker <walkernelsoncode@gmail.com>
2021-10-01 17:58:51 +1000

    trying to trigger code linter

--- config/middleware.js
+++ config/middleware.js
@@ -3,9 +3,7 @@ const secret = process.env.PASSPORT_KEY;
 
 const withAuth = function(req, res, next) {
   const token = req.cookies.token;  
-  if (!token) {
-    res.status(401).send('Unauthorized: No token provided');
-  } 
+  if (!token) {    res.status(401).send('Unauthorized: No token provided');  } 
   else {
     jwt.verify(token, secret, function(err, decoded) {
       if (err) {
