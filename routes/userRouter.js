058f16e39276ab46368bab10e58daf71c022dbf1
jackwhitehead <whiteheadjack@hotmail.com>
2021-10-01 19:20:57 +1000

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,19 +8,19 @@ const withAuth = require('../config/middleware').withAuth
 const userRouter = express.Router()
 const user = require('../controllers/userController')
 
-userRouter.post("/api/login", urlencodedParser, user.login);
+userRouter.post('/api/login', urlencodedParser, user.login)
 
-userRouter.post('/api/signup', urlencodedParser, user.registerUser);
+userRouter.post('/api/signup', urlencodedParser, user.registerUser)
 
-userRouter.get('/api/secret', withAuth, function(req, res) {
-    res.send('The password is potato');
-});
-
-userRouter.get('/verify', withAuth, function(req, res) {
-    res.sendStatus(200);
-});
-
-userRouter.get('/home', withAuth, function(req, res) {
-    res.send('hello');
+userRouter.get('/api/secret', withAuth, function (req, res) {
+  res.send('The password is potato')
 })
-module.exports = userRouter;
+
+userRouter.get('/verify', withAuth, function (req, res) {
+  res.sendStatus(200)
+})
+
+userRouter.get('/home', withAuth, function (req, res) {
+  res.send('hello')
+})
+module.exports = userRouter
