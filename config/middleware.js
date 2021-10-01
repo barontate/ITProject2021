a33a9d4b1a2e7568406420e902dc5d29af905e34
nelsonwalker <walkernelsoncode@gmail.com>
2021-10-01 18:26:11 +1000

    fixed middleware.js

--- config/middleware.js
+++ config/middleware.js
@@ -3,19 +3,16 @@ const secret = process.env.PASSPORT_KEY;
 
 const withAuth = function(req, res, next) {
   const token = req.cookies.token;  
-  if (!token) {    res.status(401).send('Unauthorized: No token provided');  } 
+  if (!token) {
+    res.status(401).send('Unauthorized: No token provided');
+  } 
   else {
     jwt.verify(token, secret, function(err, decoded) {
-
-      if (err) { res.status(401).send('Unauthorized: Invalid token');
-
-
+      if (err) {
+        res.status(401).send('Unauthorized: Invalid token');
       } 
       else {
         req.email = decoded.email;
-
-
-
         next();
       }
     });
