6bed1fa3874e03d47db488722fd94607f9cd3838
nelsonwalker <walkernelsoncode@gmail.com>
2021-10-17 16:18:49 +1100

    added new contacts for demo

--- client/src/features/contacts/ContactSlice.js
+++ client/src/features/contacts/ContactSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
-  { id: '1', firstName: 'Jon', lastName: 'Doe', highlight: "From uni", notes: "All round good bloke"},
-  { id: '2', firstName: 'Jeff', highlight: "Good at haskell", lastName: 'Wiggim' },
-  { id: '3', firstName: 'Jeff', lastName: 'Wiggim' },
-  { id: '4', firstName: 'Nelson', highlight: "Has a boat", lastName: 'Walker'},
-  { id: '5', firstName: 'Jon', highlight: "From school", lastName: 'Doe'},
-  { id: '6', firstName: 'Jeff', lastName: 'Wiggim' },
-  { id: '7', firstName: 'Jeff', lastName: 'Wiggim' },
-  { id: '8', firstName: 'Nelson', lastName: 'Walker'},
-  { id: '9', firstName: 'Jon', lastName: 'Doe'},
-  { id: '10', firstName: 'Jeff', lastName: 'Wiggim' },
+  { id: '1', firstName: 'Lawson', lastName: 'Pegler', highlight: "IT Project team leader", notes: "Loves dinosaurs. Third year unimelb student, does tutoring."},
+  { id: '2', firstName: 'Jack', lastName: 'Whitehead', highlight: "Good music taste", notes: "Knowledgeable when it comes to API's. Originally from Darwin"},
+  { id: '3', firstName: 'Nic', lastName: 'Rickard', highlight: "Rovers guy", notes: "Professional Dota 2 Player" },
+  { id: '4', firstName: 'Nelson', highlight: "Has a boat", lastName: 'Walker', notes: "3rd year uni, breaking bad fan"},
+  { id: '5', firstName: 'Tate', highlight: "Works for Government", lastName: 'de Klerk', notes: "Has a sweet car"},
+  { id: '6', firstName: 'Lance', lastName: 'Franklin', highlight: "excellent front-end dev", notes: "Met him at brownlow medal night 2018. Great guy, kicks a lot of goals." },
+  { id: '7', firstName: 'Alan', lastName: 'Watts', highlight: "Motivational consultant", notes: "Very wise man, met him on a trip to India." },
+  { id: '8', firstName: 'John', lastName: 'Bonham', highlight: "SQL expert", notes: "Great drummer and computer scientist."},
+  { id: '9', firstName: 'Christian', lastName: 'Petracca', highlight: "Agile expert, automates tasks with python", notes: "Met through 2021 premiership celebrations in 2021"},
+  { id: '10', firstName: 'Walter', lastName: 'White', highlight: "excellent teacher", notes: "met in highschool" },
 ]
 
 const contactSlice = createSlice({
